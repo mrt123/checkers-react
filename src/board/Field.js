25c4495e3a1eb209f89de061a2a5d87e8875b8d5
Tomasz <tomasz.szawara@gmail.com>
2019-09-11 10:52:06 +0200

    added Field.js

---
+++ src/board/Field.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PinBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 60px;
+  height: 60px;
+  background-color: ${p => p.color};
+  color: red;
+  font-size: 9px;
+  overflow-wrap: break-word;
+  word-break: break-all;
+`;
+
+export default ({ def }) => {
+  return (
+    <PinBox color={def.color}>
+      {def.y} - {def.x}
+    </PinBox>
+  );
+};
