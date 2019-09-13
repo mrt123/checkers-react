445f990f48257ae44473fdbd674a7052221383e2
Tomasz <tomasz.szawara@gmail.com>
2019-09-13 14:20:18 +0200

    Updates

---
+++ precache-manifest.cbafd13e2bb9fcc8c966c91e789c60fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b89d3addc587729b73bc18df3c19912",
+    "url": "/checkers-react/index.html"
+  },
+  {
+    "revision": "0a934e40294df2198fe7",
+    "url": "/checkers-react/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "c25bbbe7641ec747a3f8",
+    "url": "/checkers-react/static/css/main.54f545a7.chunk.css"
+  },
+  {
+    "revision": "0a934e40294df2198fe7",
+    "url": "/checkers-react/static/js/2.60868356.chunk.js"
+  },
+  {
+    "revision": "c25bbbe7641ec747a3f8",
+    "url": "/checkers-react/static/js/main.1c98cf91.chunk.js"
+  },
+  {
+    "revision": "5f4e1a8ca1d9b68b33e2",
+    "url": "/checkers-react/static/js/runtime~main.8895a125.js"
+  }
+]);
