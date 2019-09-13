f19a41e2a3309ef4660b17c18da9b8d9a7de296b
Tomasz <tomasz.szawara@gmail.com>
2019-09-13 18:06:15 +0200

    player turns, and consecutive jumps

--- src/gameApi/boardApi.js
+++ src/gameApi/boardApi.js
@@ -1,14 +1,18 @@
-export function tryMove(pin, fieldTarget, fields) {
-  if (fieldTarget.pin !== null || fieldTarget.color === 'white') return { type: 'illegal' };
+export function tryMove(pin, fieldTarget, fields, activePlayer) {
+  const illegalMove = { type: 'illegal', updatedPin: pin };
+
+  if (fieldTarget.pin !== null || fieldTarget.color === 'white' || activePlayer.color !== pin.color) return illegalMove;
 
   const pinX = pin.x;
   const pinY = pin.y;
 
   const moveCountX = Math.abs(fieldTarget.x - pinX);
   const moveCountY = fieldTarget.y - pinY;
+  const updatedPin = { ...pin, x: fieldTarget.x, y: fieldTarget.y };
 
-  if (moveCountX === 1 && moveCountY === pin.moveDirection * 1) return { type: 'normal' };
-  else if (moveCountX === 2 && moveCountY === pin.moveDirection * 2) {
+  if (moveCountX === 1 && moveCountY === pin.moveDirection * 1) {
+    return { type: 'normal', updatedPin };
+  } else if (moveCountX === 2 && moveCountY === pin.moveDirection * 2) {
     const intermediateX = pinX + (fieldTarget.x - pinX) / 2;
     const intermediateY = pinY + (fieldTarget.y - pinY) / 2;
 
@@ -17,8 +21,35 @@ export function tryMove(pin, fieldTarget, fields) {
     })[0];
 
     if (intermediateField.pin && intermediateField.pin.color !== pin.color) {
-      return { type: 'kill', killed: intermediateField.pin };
-    } else return { type: 'illegal' };
-  } else return { type: 'illegal' };
-  //TODO: other cases
+      return { type: 'kill', killed: intermediateField.pin, updatedPin };
+    } else return illegalMove;
+  } else return illegalMove;
+}
+
+// must try two fields diagonally forward
+export function additionalJumpPossible(pin, fields) {
+  // must try with last active pin
+
+  const target1 = {
+    x: pin.x + 2,
+    y: pin.y + pin.moveDirection * 2
+  };
+
+  const target2 = {
+    x: pin.x - 2,
+    y: pin.y + pin.moveDirection * 2
+  };
+
+  const fieldTarget1 = fields.filter(f => f.x === target1.x && f.y === target1.y)[0];
+  const fieldTarget2 = fields.filter(f => f.x === target2.x && f.y === target2.y)[0];
+
+  console.log({ fieldTarget2 });
+
+  let move1Result, move2Result;
+
+  if (fieldTarget1 !== undefined) move1Result = tryMove(pin, fieldTarget1, fields, { color: pin.color });
+  if (fieldTarget2 !== undefined) move2Result = tryMove(pin, fieldTarget2, fields, { color: pin.color });
+  const possibleOutcomes = [move1Result, move2Result].filter(mr => mr !== undefined && mr.type !== 'illegal');
+  console.log({ possibleOutcomes });
+  return possibleOutcomes.length > 0;
 }
--- src/state/board/boardReducer.js
+++ src/state/board/boardReducer.js
@@ -1,23 +1,29 @@
 import { SET_ACTIVE_PIN, SET_HIGHLIGHT_FIELD, UNSET_HIGHLIGHT_FIELD, TRY_MOVE_PIN } from '../board/boardActions.js';
 import { generateFields } from '../../gameApi/fieldsApi.js';
-import { tryMove } from '../../gameApi/boardApi.js';
+import { tryMove, additionalJumpPossible } from '../../gameApi/boardApi.js';
 
 const board = (
   state = {
+    activePlayer: {
+      color: 'orange'
+    },
     activePin: null,
+    blockActivePin: false,
     fields: generateFields()
   },
   action
 ) => {
   switch (action.type) {
     case SET_ACTIVE_PIN: {
-      return {
-        ...state,
-        activePin: action.pin
-      };
+      if (state.blockActivePin) return state;
+      else
+        return {
+          ...state,
+          activePin: action.pin
+        };
     }
     case SET_HIGHLIGHT_FIELD: {
-      const moveResult = tryMove(state.activePin, action.f, state.fields);
+      const moveResult = tryMove(state.activePin, action.f, state.fields, state.activePlayer);
 
       if (moveResult.type === 'illegal') return state;
       else {
@@ -36,23 +42,45 @@ const board = (
     case TRY_MOVE_PIN: {
       const targetField = action.f;
       const pin = state.activePin;
-      const moveResult = tryMove(pin, targetField, state.fields);
+      const moveResult = tryMove(pin, targetField, state.fields, state.activePlayer);
 
       if (moveResult.type === 'illegal') return state;
       else {
-        return {
-          ...state,
-          fields: state.fields.map(f => {
-            if (moveResult.killed === f.pin) return { ...f, pin: null };
+        // todo: move newFields gen to tryMove() as updatedFields
+        const newFields = state.fields.map(f => {
+          if (moveResult.killed === f.pin) return { ...f, pin: null };
 
-            if (f.x === pin.x && f.y === pin.y) return { ...f, pin: null };
-            else if (f === targetField) {
-              const updatedPin = { ...pin, x: f.x, y: f.y };
-              return { ...f, pin: updatedPin };
-            }
-            return f;
-          })
+          if (f.x === pin.x && f.y === pin.y) return { ...f, pin: null };
+          else if (f === targetField) {
+            return { ...f, pin: moveResult.updatedPin };
+          }
+          return f;
+        });
+
+        const statePart = {
+          ...state,
+          fields: newFields,
+          activePin: moveResult.updatedPin
         };
+
+        const activePlayer = state.activePlayer;
+        const otherPlayer = getOtherPlayer(activePlayer);
+
+        if (moveResult.type === 'kill') {
+          const isAdditionalJumpPossible = additionalJumpPossible(moveResult.updatedPin, newFields);
+
+          return {
+            ...statePart,
+            blockActivePin: isAdditionalJumpPossible,
+            activePlayer: isAdditionalJumpPossible ? activePlayer : otherPlayer
+          };
+        } else {
+          return {
+            ...statePart,
+            blockActivePin: false,
+            activePlayer: otherPlayer
+          };
+        }
       }
     }
     default: {
@@ -66,6 +94,17 @@ function highlightOneField(fields, targetField, hlValue) {
     const fieldMatch = f.x === targetField.x && f.y === targetField.y;
     return fieldMatch ? { ...f, highlight: hlValue } : f;
   });
+}
+
+function getNextPlayerAfterKill(updatedPin, fields) {
+  const activePlayer = { color: updatedPin.color };
+  return additionalJumpPossible(updatedPin, fields) ? activePlayer : getOtherPlayer(activePlayer);
+}
+
+function getOtherPlayer(player) {
+  return {
+    color: player.color === 'orange' ? 'white' : 'orange'
+  };
 }
 
 export default board;
