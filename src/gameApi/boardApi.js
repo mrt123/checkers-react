a55ad78e912c8c4d78efde08637755eae95e988c
Tomasz <tomasz.szawara@gmail.com>
2019-09-14 11:03:58 +0200

    refactored, removed highlight from loggers

--- src/gameApi/boardApi.js
+++ src/gameApi/boardApi.js
@@ -3,18 +3,17 @@ export function tryMove(pin, fieldTarget, fields, activePlayer, consecutiveMove)
 
   if (fieldTarget.pin !== null || fieldTarget.color === 'white' || activePlayer.color !== pin.color) return illegalMove;
 
-  const pinX = pin.x;
-  const pinY = pin.y;
+  const moveDiffX = fieldTarget.x - pin.x;
+  const moveDiffY = fieldTarget.y - pin.y;
+  const moveCountX = Math.abs(moveDiffX);
 
-  const moveCountX = Math.abs(fieldTarget.x - pinX);
-  const moveCountY = fieldTarget.y - pinY;
   const updatedPin = { ...pin, x: fieldTarget.x, y: fieldTarget.y };
 
-  if (moveCountX === 1 && moveCountY === pin.moveDirection * 1 && !consecutiveMove) {
+  if (moveCountX === 1 && moveDiffY === pin.moveDirection * 1 && !consecutiveMove) {
     return { type: 'normal', updatedPin };
-  } else if (moveCountX === 2 && moveCountY === pin.moveDirection * 2) {
-    const intermediateX = pinX + (fieldTarget.x - pinX) / 2;
-    const intermediateY = pinY + (fieldTarget.y - pinY) / 2;
+  } else if (moveCountX === 2 && moveDiffY === pin.moveDirection * 2) {
+    const intermediateX = pin.x + moveDiffX / 2;
+    const intermediateY = pin.y + moveDiffY / 2;
 
     const intermediateField = fields.filter(f => {
       return f.x === intermediateX && f.y === intermediateY;
@@ -26,10 +25,7 @@ export function tryMove(pin, fieldTarget, fields, activePlayer, consecutiveMove)
   } else return illegalMove;
 }
 
-// must try two fields diagonally forward
 export function additionalJumpPossible(pin, fields) {
-  // must try with last active pin
-
   const target1 = {
     x: pin.x + 2,
     y: pin.y + pin.moveDirection * 2
@@ -40,16 +36,15 @@ export function additionalJumpPossible(pin, fields) {
     y: pin.y + pin.moveDirection * 2
   };
 
+  // TODO: shouldn't Fields, and Field be a Class ? [getField(x,y), cleanField(x,y), getPin(x,y)]
   const fieldTarget1 = fields.filter(f => f.x === target1.x && f.y === target1.y)[0];
   const fieldTarget2 = fields.filter(f => f.x === target2.x && f.y === target2.y)[0];
-
-  console.log({ fieldTarget2 });
 
   let move1Result, move2Result;
 
   if (fieldTarget1 !== undefined) move1Result = tryMove(pin, fieldTarget1, fields, { color: pin.color });
   if (fieldTarget2 !== undefined) move2Result = tryMove(pin, fieldTarget2, fields, { color: pin.color });
   const possibleOutcomes = [move1Result, move2Result].filter(mr => mr !== undefined && mr.type !== 'illegal');
-  console.log({ possibleOutcomes });
+
   return possibleOutcomes.length > 0;
 }
--- src/state/store.js
+++ src/state/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
+import { SET_HIGHLIGHT_FIELD, UNSET_HIGHLIGHT_FIELD } from './board/boardActions.js';
 
 const middleWares = [];
 
@@ -8,7 +9,10 @@ const middleWares = [];
 const logsOn = true;
 if (logsOn) {
   const loggerMiddleware = createLogger({
-    collapsed: true
+    collapsed: true,
+    predicate: (getState, action) => {
+      return action.type !== SET_HIGHLIGHT_FIELD && action.type !== UNSET_HIGHLIGHT_FIELD;
+    }
   });
   middleWares.push(loggerMiddleware);
 }
